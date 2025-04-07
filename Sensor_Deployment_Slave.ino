b52b6c4ec68695b4e1527816aa68c8aeb71bb40f
jacksonsherwood <jdsherwood23@my.trine.edu>
2025-04-07 19:49:23 -0400

    Sensor_Deployment_Slave.ino

--- Sensor_Deployment_Slave.ino
+++ Sensor_Deployment_Slave.ino
@@ -5,6 +5,7 @@
 int myData;
 Servo servo;
 const int servoPin = 18;
+const int relayPin = 6;
 
 //callback function that will be executed when data is received
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
@@ -50,6 +51,7 @@ void setup() {
   esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
 
   servo.attach(servoPin, 500, 2500);
+  pinMode(relayPin, OUTPUT);
 }
  
 void loop() {
@@ -59,5 +61,10 @@ void loop() {
     close();
     myData = 0;
     Serial.println(myData);
+  } else if (myData == 5){
+    digitalWrite(relayPin, HIGH);
+    delay(5000);
+    digitalWrite(relayPin, LOW);
+    myData = 0;
   }
 }
